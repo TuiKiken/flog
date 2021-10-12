f8c436eb3d631154607199b3a4612897553ee0f2
Valentin Shevko <vshevko@viber.com>
2021-10-12 11:52:13 +0300

    fix deploy

---
+++ src/configs/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBDl2F8VOeyBuwX1X4hXLiwTbqVxup2UlM",
+  authDomain: "flog-291321.firebaseapp.com",
+  projectId: "flog-291321",
+  storageBucket: "flog-291321.appspot.com",
+  messagingSenderId: "643571490720",
+  appId: "1:643571490720:web:1d4945d269dd3a76b88ec5"
+};
