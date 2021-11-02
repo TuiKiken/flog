408bfd38a0ad90c75cc0f9d733150dd429867e55
Valentin Shevko <vshevko@viber.com>
2021-11-02 03:19:21 +0300

    add create and get flogs services

--- src/sagas/flog.ts
+++ src/sagas/flog.ts
@@ -3,12 +3,12 @@ import { all, call, put, takeLatest } from 'redux-saga/effects';
 import { Saga } from 'types/saga';
 import { AddFlogRequest, addFlogRequest } from 'store/flog';
 import { MESSAGE_TYPE, showMessageRequest } from 'store/message';
-import { addFlog } from 'services/flog';
+import { addFlogItem } from 'services/flog';
 
 const addFlogRequestHandler: Saga<AddFlogRequest> = function* ({ payload }) {
   const { data } = payload;
 
-  yield call(addFlog, data);
+  yield call(addFlogItem, data);
   yield put(showMessageRequest({
     type: MESSAGE_TYPE.SUCCESS,
     message: 'Отчёт о рыбалке добавлен',
--- src/services/flog.ts
+++ src/services/flog.ts
@@ -1,10 +1,26 @@
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs, query, where, serverTimestamp } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 import { FlogData } from 'types/flog';
 import { firebaseFirestore } from 'services/firebase';
 
-export const addFlog = async (data: FlogData) => {
-  const docRef = await addDoc(collection(firebaseFirestore, 'fishing_log'), data);
+export const addFlogItem = async (data: FlogData) => {
+  const user = getAuth().currentUser;
+  const flogCollectionRef = collection(firebaseFirestore, 'fishing_log');
+  const docRef = await addDoc(flogCollectionRef, {
+    ...data,
+    author_uid: user?.uid,
+    timestamp: serverTimestamp(),
+  });
 
   return docRef.id
+};
+
+export const getFlogList = async () => {
+  const user = getAuth().currentUser;
+  const flogCollectionRef = collection(firebaseFirestore, 'fishing_log');
+  const docsQuery = query(flogCollectionRef, where('author_uid', '==', user?.uid))
+  const docsRef = await getDocs(docsQuery);
+
+  return docsRef.docs.map(doc => ({ ...doc.data(), id: doc.id }));
 };
